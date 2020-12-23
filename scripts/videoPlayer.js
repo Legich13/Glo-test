bf3c8013e570a7c3ec84a704446bf40f9c5b6d7c
Legich13 <60274918+Legich13@users.noreply.github.com>
2020-12-23 16:56:42 +0300

    Add files via upload

---
+++ scripts/videoPlayer.js
@@ -0,0 +1,92 @@
+export const videoPlayerInit = () => {
+  /*  video-player
+    video-button__play
+    video-button__stop
+    video-time__passed
+    video-progress
+    video-time__total
+    */
+
+    const videoPlayer = document.querySelector('.video-player')
+    const videoButtonPlay = document.querySelector('.video-button__play')
+    const videoButtonStop = document.querySelector('.video-button__stop')
+    const videoProgress = document.querySelector('.video-progress')
+    const videoTimePassed = document.querySelector('.video-time__passed')
+    const videoVolume = document.querySelector('.video-volume')
+    const videoTimeTotal = document.querySelector('.video-time__total')
+
+
+    const toggleIcon = () => {
+        if(videoPlayer.paused){
+            videoButtonPlay.classList.remove('fa-pause')
+            videoButtonPlay.classList.add('fa-pause')
+        } else {
+            videoButtonPlay.classList.add('fa-pause')
+            videoButtonPlay.classList.remove('fa-pause')
+        }
+    }
+
+
+    const togglePlay = () => {
+        if(videoPlayer.paused){
+            videoPlayer.play()
+        } else {
+            videoPlayer.pause()
+        }
+
+        toggleIcon()
+      
+    } 
+
+        const stopPlay = () => {
+        videoPlayer.pause()
+        videoPlayer.currentTime = 0
+    } 
+
+    const addZero = n => n < 10 ? '0' + n : n
+    
+    
+    videoPlayer.addEventListener('click', togglePlay)
+    videoButtonPlay.addEventListener('click', togglePlay)
+
+    videoPlayer.addEventListener('play', toggleIcon)
+    videoPlayer.addEventListener('pause', toggleIcon)
+
+    videoButtonStop.addEventListener('click', stopPlay)
+
+
+    videoPlayer.addEventListener('timeupdate', () => {
+        const currentTime = videoPlayer.currentTime
+        const duration = videoPlayer.duration 
+
+        videoProgress.value = (currentTime / duration ) * 100
+        
+        let minutePassed = Math.floor(currentTime / 60)
+        let secondsPassed = Math.floor(currentTime % 60)
+
+        let minuteTotal = Math.floor(duration / 60)
+        let secondsTotal = Math.floor(duration % 60)
+
+     //   videoTimePassed.textContent = addZero(minutePassed) + ':' +  addZero(secondsPassed)
+     //   videoTimeTotal.textContent = addZero(minuteTotal) + ':' + addZero(secondsTotal)
+
+        videoTimePassed.textContent = `${addZero(minutePassed)}:${addZero(secondsPassed)}`
+        videoTimeTotal.textContent = `${addZero(minuteTotal)}:${addZero(secondsTotal)}`
+    })
+
+    videoProgress.addEventListener('change', () => {
+        const duration = videoPlayer.duration 
+        const value = videoProgress.value
+
+        videoPlayer.currentTime = (value * duration) / 100
+    })
+
+    videoVolume.addEventListener('input' , () => {
+        videoPlayer.volume = videoVolume.value / 100
+    }) 
+
+    videoPlayer.volume = 0.5
+    videoPlayer.volume  =  videoPlayer.volume  * 100
+    
+}
+
